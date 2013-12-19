a4dd13c6c6fabb138c964d1128b5919af320c65e
Alexander Böhn <fish2000@gmail.com>
2013-12-19 04:21:30 -0500

    Finally cleaned up and converted viron.py to use the futuristic print function

--- src/viron/viron.py
+++ src/viron/viron.py
@@ -27,6 +27,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 """
+from __future__ import print_function
 
 import sys
 import os
@@ -34,55 +35,68 @@ import re
 from string import Template
 from argh import ArghParser, command, arg
 
-
 @command
 def viron(swaptext, swapdic=os.environ, ignoreseq=tuple(),
     dumpstride=3, warn_unmapped_labels=True):
-
     """ Simplistically replace any and all environment-variable-esque
-    $VAR_LABEL declarations in :`swaptext` with content, as per the
-    :`swapdic` mapping, *qua* said declared labels. """
+        $VAR_LABEL declarations in :`swaptext` with content from
+        appropriate :`swapdic` mappings, *qua* said declared labels. """
 
     matcher = r'(?<!\\)\${1}([A-Z][A-Z0-9_]+)'
     bracedmatcher = r'(?<!\\)\${1}\{([A-Z][A-Z0-9_]+)\}'
     swapmapper = lambda en: en and (en, swapdic.get(en, "$%s" % en)) or ''
     swapfilter = lambda ex: ex and not (ex in ignoreseq)
     
-    envs = dict(map(swapmapper, filter(swapfilter,
-        re.compile(matcher).findall(swaptext))))
-    envs.update(dict(map(swapmapper, filter(swapfilter,
-        re.compile(bracedmatcher).findall(swaptext)))))
-
+    envs = dict(map(swapmapper,
+        filter(swapfilter,
+            re.compile(matcher).findall(swaptext))))
+    envs.update(
+        dict(map(swapmapper,
+            filter(swapfilter,
+                re.compile(bracedmatcher).findall(swaptext)))))
     strays = set(envs.keys()).difference(swapdic.keys())
 
     if warn_unmapped_labels and len(strays) > 0:
+        
         for stray in strays:
-            print >>sys.stderr, "Warning: unmapped template label %s" % stray
+            print("Warning: unmapped template label %s" % stray,
+                file=sys.stderr)
+        
         valids = sorted(filter(
             lambda s: s.isupper() and not s.startswith('_'),
                 swapdic.keys()), key=len)
         strident = "%%-%ds" % len(list(reversed(valids))[0])
         stride = xrange(0, len(valids), dumpstride)
-        print >>sys.stderr, "Valid template labels:\n"
+        
+        print("Valid template labels:\n",
+            file=sys.stderr)
         for stride in [valids[i:dumpstride+i] for i in stride]:
-            print >>sys.stderr, ": ".join([strident % step for step in stride])
+            print(": ".join(
+                [strident % step for step in stride]),
+                    file=sys.stderr)
 
     return Template(
-        re.sub(r'(\$+?)(?![A-Z0-9\{]+?)', '\g<0>\g<1>', swaptext,
-            flags=re.MULTILINE)).safe_substitute(**envs)
+        re.sub(r'(\$+?)(?![A-Z0-9\{]+?)',
+            '\g<0>\g<1>', swaptext,
+                flags=re.MULTILINE)).safe_substitute(**envs)
 
 
 def filepath(pth, ignoreseq=tuple(),
     dumpstride=3, warn_unmapped_labels=True):
+    """ Perform viron() substitution with file contents. """
+    
     try:
         with open(pth, 'r') as filepth:
             return viron(filepth.read(),
                 ignoreseq=ignoreseq,
                 dumpstride=dumpstride,
                 warn_unmapped_labels=warn_unmapped_labels)
+    
     except IOError, err:
-        print >>sys.stderr, "File path %s is totally invalid." % pth
-        print >>sys.stderr, unicode(err).encode('UTF-8')
+        print("File path %s is totally invalid." % pth,
+            file=sys.stderr)
+        print(unicode(err).encode('UTF-8'),
+            file=sys.stderr)
         sys.exit(1)
 
 
@@ -96,10 +110,11 @@ def stdinput(ignoreseq=tuple(), dumpstride=3):
         if not line:
             break
 
-        sys.stdout.write(viron(line,
-            ignoreseq=ignoreseq,
-            dumpstride=dumpstride,
-            warn_unmapped_labels=False))
+        sys.stdout.write(
+            viron(line,
+                ignoreseq=ignoreseq,
+                dumpstride=dumpstride,
+                warn_unmapped_labels=False))
     return ''
 
 @arg('TEXT', nargs='?', default=None,
@@ -115,24 +130,36 @@ def stdinput(ignoreseq=tuple(), dumpstride=3):
 def text(args):
     warn_unmapped_labels = (not args.quiet)
     ignoreseq = tuple(str(args.ignore).upper().split(','))
+    
     if args.dump_stride < 1:
         warn_unmapped_labels = False
+    
     if args.TEXT is None:
+    
         if args.file is None:
-            return stdinput(ignoreseq=ignoreseq,
+            return stdinput(
+                ignoreseq=ignoreseq,
                 dumpstride=args.dump_stride)
+    
         else:
             return filepath(args.file,
                 ignoreseq=ignoreseq,
                 dumpstride=args.dump_stride,
                 warn_unmapped_labels=warn_unmapped_labels)
+    
     else:
+    
         if args.file is not None:
-            print >>sys.stderr, "Warning: reading from %s (ignoring text '%s')" % (args.file, args.TEXT)
-            return filepath(args.file, ignoreseq=ignoreseq,
+            print("Warning: reading from %s (ignoring text '%s')" % (
+                args.file, args.TEXT),
+                file=sys.stderr)
+            return filepath(args.file,
+                ignoreseq=ignoreseq,
                 dumpstride=args.dump_stride)
+    
         else:
-            return viron(args.TEXT, ignoreseq=ignoreseq,
+            return viron(args.TEXT,
+                ignoreseq=ignoreseq,
                 dumpstride=args.dump_stride,
                 warn_unmapped_labels=warn_unmapped_labels)
 
@@ -143,12 +170,15 @@ p.add_commands([text, filepath])
 def main():
     argv = list(sys.argv[1:])
     cmdarg = argv[0:1]
+    
     if len(cmdarg) > 0:
         cmd = cmdarg[0]
     else:
         cmd = None
+    
     if cmd is not None and cmd not in ('text', 'filepath'):
         argv.insert(0, 'text')
+    
     p.dispatch(argv=argv)
 
 if __name__ == "__main__":
